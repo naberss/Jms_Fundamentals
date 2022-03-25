e1cb22198c2f43c211b9c0b844225d4fc0feec08
naberss <37065937+naberss@users.noreply.github.com>
2022-03-25 19:52:19 -0300

    [#2] - Consumer || Closing Components
    - Definindo um consumer.
    
    - Configurando a conexão corretamente para que a mesma passe a receber as mensagens enviadas.
    
    -Fechando Context e Connection ao final do codigo.

--- src/main/java/com/nabers/jms/basics/FirstQueue_JMS1X.java
+++ src/main/java/com/nabers/jms/basics/FirstQueue_JMS1X.java
@@ -3,6 +3,7 @@ package com.nabers.jms.basics;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.Session;
@@ -10,18 +11,20 @@ import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
-public class FirstQueue {
-    public static void main(String[] args) {
+public class FirstQueue_JMS1X {
+    public static void main(String[] args) throws NamingException, JMSException {
+        InitialContext initialContext = null;
+        Connection connection = null;
         try {
 
             //Cria contexto inicial baseado na propriedade "java.naming.factory.initial" da config. "jndi.properties".
-            InitialContext initialContext = new InitialContext();
+            initialContext = new InitialContext();
 
             //Criando ConnectionFactory, usando o contexto, buscando na api JNDI pelo recurso de nome "ConnectionFactory".
             ConnectionFactory connectionFactory = (ConnectionFactory) initialContext.lookup("ConnectionFactory");
 
             //Intanciando uma conexao.
-            Connection connection = connectionFactory.createConnection();
+            connection = connectionFactory.createConnection();
 
             //Criando sessao.
             Session session = connection.createSession();
@@ -37,11 +40,32 @@ public class FirstQueue {
             producer.send(message);
 
 
+            //------------------------------------//
+
+            //Criando consumer e definindo o local de onde as mensagens deverao ser consumidas.
+            MessageConsumer consumer = session.createConsumer(queue);
+
+            //Inicia a entrega de mensagens recebidas ao consumer.
+            connection.start();
+
+            TextMessage receivedMessage = (TextMessage) consumer.receive();
+
+            System.out.println(receivedMessage.getText());
+
 
         } catch (NamingException e) {
             e.printStackTrace();
         } catch (JMSException e) {
             e.printStackTrace();
+        } finally {
+            if (initialContext != null) {
+                initialContext.close();
+            }
+
+            if (connection != null) {
+                connection.close();
+            }
+
         }
 
     }
