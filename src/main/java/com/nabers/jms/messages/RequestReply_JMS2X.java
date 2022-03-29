7145a4244129f5264bf6d3c0870bed5239bf1203
naberss <37065937+naberss@users.noreply.github.com>
2022-03-29 16:52:03 -0300

    [#5] - Request-Reply correlation
    - Correlacionando responses com as mensagens que originaram a requisição.

--- src/main/java/com/nabers/jms/messages/RequestReply_JMS2X.java
+++ src/main/java/com/nabers/jms/messages/RequestReply_JMS2X.java
@@ -29,29 +29,42 @@ public class RequestReply_JMS2X {
 
              /* toda mensagem enviada a partir desta queue, possuira uma propriedade de nome 'JMSReplyTo'
                  que bascicamente, armazena o destino da response (reply) para onde a mesma deve ser enviada */
-
             requestProducer.setJMSReplyTo(replyQueue);
 
             TextMessage requestMessage = jmsContext.createTextMessage("io");
+
+             /* Setando o header replyTo a nivel da mensagem, mas so seria necessario caso o mesmo ja
+                 nao estivesse sendo definido a nivel do producer */
             requestMessage.setJMSReplyTo(replyQueue);
 
             requestProducer.send(requestQueue, requestMessage);
+
+            System.out.println("ID: "+requestMessage.getJMSMessageID());
 
             JMSConsumer requestConsumer = jmsContext.createConsumer(requestQueue);
 
             System.out.println(requestConsumer.receiveBody(String.class));
 
             //-----------------------------------------------------------//
-
             JMSProducer replyProducer = jmsContext.createProducer();
 
-            /* Envia a confirmação de chegada da mensagem a partir da propriedade de reply incluida no header */
-            replyProducer.send(requestMessage.getJMSReplyTo(),"got u");
+            TextMessage replyMessage = jmsContext.createTextMessage("got u");
 
+            /* Correlaciona a mensagem de resposta (reply msg) ao envio inicial (request msg),
+                 vide ID da mensagem consumida (request msg) */
+            replyMessage.setJMSCorrelationID(requestMessage.getJMSMessageID());
+
+            /* Envia a confirmação de chegada da mensagem a partir da propriedade de replyTo incluida no header */
+            replyProducer.send(requestMessage.getJMSReplyTo(),replyMessage);
+
+
+            // Consome a mensagem de resposta
             JMSConsumer replyConsumer = jmsContext.createConsumer(replyQueue);
-            System.out.println(replyConsumer.receiveBody(String.class));
 
+            TextMessage ConsumedMsg = (TextMessage) replyConsumer.receive();
 
+            System.out.println("Text: "+ConsumedMsg.getText());
+            System.out.println("CorrelationID: "+ConsumedMsg.getJMSCorrelationID());
 
         }
 
